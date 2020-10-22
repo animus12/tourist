2acd5c110b79a735c5e10cc52e34dfbf532c8c92
Ace <mypersonalacount4@gmail.com>
2020-10-22 22:12:26 +0800

    sving

--- tourist_management/settings.py
+++ tourist_management/settings.py
@@ -77,9 +77,13 @@ WSGI_APPLICATION = 'tourist_management.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+     'default': {
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'testdb',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
